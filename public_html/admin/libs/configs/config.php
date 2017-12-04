685269ce9bf2ff70b8d14a3a8f9511f9fedde44b
BrownySprite47 <kr0085@mail.ru>
2017-12-04 14:06:28 +0400

    Обновила подключение к БД

--- public_html/admin/libs/configs/config.php
+++ public_html/admin/libs/configs/config.php
@@ -7,20 +7,20 @@ mb_internal_encoding("utf-8");
 //paths
 
 // define("PROJECT_BACKEND_PATH", "/home/virtwww/w_soldb_33c91896/http/");
-define("PROJECT_BACKEND_PATH", "/home/s/suppor1k/vtemp.ru/public_html/admin/");
+define("PROJECT_BACKEND_PATH", "C:/OSPanel/domains/localhost/sol.loc/public_html/admin/");
 define("CONFIGS_PATH", PROJECT_BACKEND_PATH . "libs/configs/");
 define("LIBS_PATH", PROJECT_BACKEND_PATH . "libs/");
 
 //URLs
 
 // define("PROJECT_BACKEND_URL", "https://www.soldb.ru/");
-define("PROJECT_BACKEND_URL", "http://suppor1k.beget.tech/admin/");
+define("PROJECT_BACKEND_URL", "http://sol.loc/admin/");
 
 //DB
 define("DB_HOST", "localhost");
 define("DB_USER", "suppor1k_vtemp");
 define("DB_PASSWORD", "G3xx*yWq");
-define("DB_NAME", "suppor1k_vtemp");
+define("DB_NAME", "gb_db_leaders");
 define("DB_TABLE_PREFIX", "leaders_");
 define("DB_PREFIX", DB_NAME . "." . DB_TABLE_PREFIX);
 
